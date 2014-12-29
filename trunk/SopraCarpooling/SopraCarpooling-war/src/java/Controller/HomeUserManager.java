5f9b4ae5d768b3725ff6d97392c84331ecfb9fb9
orosens@gmail.com <orosens@gmail.com@82b39981-8c08-d3b6-9424-b9cea07f54b6>
2014-12-29 14:50:22 +0000

    servlets crées avec redirection vers leurs pages

--- trunk/SopraCarpooling/SopraCarpooling-war/src/java/Controller/HomeUserManager.java
+++ trunk/SopraCarpooling/SopraCarpooling-war/src/java/Controller/HomeUserManager.java
@@ -30,18 +30,8 @@ public class HomeUserManager extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HomeUserManager</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeUserManager at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        /* TODO output your page here. You may use following sample code. */
+        this.getServletContext().getRequestDispatcher("/WEB-INF/userhome.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
