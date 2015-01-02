6aa02504a4cd364ac099b3e3d249f25c4abf5908
salahatmitim@gmail.com <salahatmitim@gmail.com@82b39981-8c08-d3b6-9424-b9cea07f54b6>
2015-01-02 14:03:29 +0000

    SearchManager fonctionne

--- trunk/SopraCarpooling/SopraCarpooling-war/src/java/Controller/SearchManager.java
+++ trunk/SopraCarpooling/SopraCarpooling-war/src/java/Controller/SearchManager.java
@@ -4,7 +4,18 @@
  * and open the template in the editor.
  */
 package Controller;
-
+import Model.DatabaseManager;
+import static Model.DatabaseManager.usersSameJourney;
+import Model.Model;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.registry.infomodel.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -18,6 +29,29 @@ import javax.servlet.http.HttpServletResponse;
  * @author Samih
  */
 public class SearchManager extends HttpServlet {
+
+    HashMap<String, String> erreurs = new HashMap<String, String>();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public void checkZip(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String zipdepart = request.getParameter("zipdepart");
+        System.out.println(zipdepart);
+        if (zipdepart != null && zipdepart.length() != 0) {
+            if (!zipdepart.matches("[0-9]{5}")) {
+                erreurs.put("zipdepart", "Code postal incorrect, veuillez réessayer");
+                request.setAttribute("erreurs", erreurs);
+            }
+        }
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -48,22 +82,22 @@ public class SearchManager extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Cookie[] cookies = request.getCookies();
-        String Valeur= null;
-        if (cookies!=null){
-        for(int i=0; i < cookies.length; i++) {
-		Cookie MonCookie = cookies[i];
+        String Valeur = null;
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie MonCookie = cookies[i];
                 if (MonCookie.getName().equals("user")) {
-			Valeur = cookies[i].getValue();
+                    Valeur = cookies[i].getValue();
                 }
+            }
         }
-        }
-        
-        if (Valeur==null){
+
+        if (Valeur == null) {
             response.sendRedirect("/SopraCarpooling-war/homeuser");
-        }else{
+        } else {
             int positionAt = Valeur.indexOf("@#**#@");
             String email = Valeur.substring(0, positionAt);
-            String password = Valeur.substring(positionAt+6);
+            String password = Valeur.substring(positionAt + 6);
             processRequest(request, response);
         }
     }
@@ -79,7 +113,35 @@ public class SearchManager extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        checkZip(request, response);
+        if (erreurs.isEmpty()) {
+            //Connection c1 = DatabaseManager.connectionDatabase();
+            ArrayList<Model.User> listUsers = new ArrayList<Model.User>();
+            //String zipcode = request.getParameter("zipcode");
+            //String sitesopra = request.getParameter("sitesopra");
+            //listUsers = usersSameJourney(c1,31400,1);
+            Model model = new Model();
+            Model.User user1 = model.new User("number1@test.com", "pass1", "USER1", "lastname1", "0606060606", 31400, 1, "6h30", "18h30", 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            Model.User user2 = model.new User("number2@test.com", "pass2", "USER2", "lastname2", "0606060606", 31400, 1, "2h30", "18h30", 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            Model.User user3 = model.new User("number3@test.com", "pass3", "USER3", "lastname3", "0606060606", 31400, 1, "9h30", "18h30", 0, 1, 1, 1, 1, 1, 1, 1, 1);
+            Model.User user4 = model.new User("number4@test.com", "pass4", "USER4", "lastname4", "0606060606", 31400, 2, "5h30", "18h30", 0, 1, 1, 1, 1, 1, 1, 1, 1);
+            Model.User user5 = model.new User("number5@test.com", "pass5", "USER5", "lastname5", "0606060606", 31600, 1, "4h30", "18h30", 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            Model.User user6 = model.new User("number6@test.com", "pass6", "USER6", "lastname6", "0606060606", 31500, 1, "7h30", "18h30", 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            Model.User user7 = model.new User("number7@test.com", "pass7", "USER7", "lastname7", "0606060606", 31400, 1, "7h30", "18h30", 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            listUsers.add(user1);
+            listUsers.add(user2);
+            listUsers.add(user3);
+            listUsers.add(user4);
+            listUsers.add(user5);
+            listUsers.add(user6);
+            listUsers.add(user7);
+            request.setAttribute("listUsers", listUsers);
+            processRequest(request, response);
+
+        } else {
+            processRequest(request, response);
+            erreurs.clear();
+        }
     }
 
     /**
