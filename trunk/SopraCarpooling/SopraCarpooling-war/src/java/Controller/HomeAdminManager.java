5baf5e8d70427593fc7a7810a9220f6c85b6745a
orosens@gmail.com <orosens@gmail.com@82b39981-8c08-d3b6-9424-b9cea07f54b6>
2015-01-09 17:27:03 +0000

    réglage bug site sopra préselectionné

--- trunk/SopraCarpooling/SopraCarpooling-war/src/java/Controller/HomeAdminManager.java
+++ trunk/SopraCarpooling/SopraCarpooling-war/src/java/Controller/HomeAdminManager.java
@@ -5,8 +5,16 @@
  */
 package Controller;
 
+import Model.DatabaseManager;
+import Model.Model;
+import Model.Model.Workplace;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -28,38 +36,51 @@ public class HomeAdminManager extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        Connection con;
+        int i = 0;
+        try {
+            con = DatabaseManager.connectionDatabase();
+            ArrayList<Model.User> listUsers = DatabaseManager.getAllUsers(con);
+            i = listUsers.size();
+        
         response.setContentType("text/html;charset=UTF-8");
         /* TODO output your page here. You may use following sample code. */
-         if (request.getParameter("disconnect")!=null){                    
-            Cookie monCookie = new Cookie("user",null) ;
+        if (request.getParameter("disconnect") != null) {
+            Cookie monCookie = new Cookie("user", null);
             monCookie.setMaxAge(0);
             response.addCookie(monCookie);
-            response.sendRedirect("/SopraCarpooling-war/");
-        }  
-         
-        else if (request.getParameter("handlePath")!=null){
-            response.sendRedirect("/SopraCarpooling-war/wrkplcelist"); 
-        }
-        /*API de création de PDF
+            response.sendRedirect("/SopraCarpooling-war/login");
+        } else if (request.getParameter("handlePath") != null) {
+            response.sendRedirect("/SopraCarpooling-war/wrkplcelist");
+        } /*API de création de PDF
          else if (request.getParameter("generateGeneralReport")!=null){
-            response.sendRedirect("/SopraCarpooling-war/persinfo"); génération d"un pdf pas de servlet
-        } */
-        else if (request.getParameter("generate")!=null){
-            response.sendRedirect("/SopraCarpooling-war/gestroutes"); 
+         response.sendRedirect("/SopraCarpooling-war/persinfo"); génération d"un pdf pas de servlet
+         } */ else if (request.getParameter("generate") != null) {
+            response.sendRedirect("/SopraCarpooling-war/gestroutes");
+        } else {
+             boolean tek=true;
+            for (int j = 0; j < i; j++) {
+                if (request.getParameter("mod" + j) != null) {
+                    Cookie monCookie = new Cookie("modifuser", listUsers.get(j).getEmail());
+                    response.addCookie(monCookie);
+                    tek=false;
+                    response.sendRedirect("/SopraCarpooling-war/useredit");
+                } else if (request.getParameter("sup" + j) != null) {
+                    DatabaseManager.deleteUsers(con, listUsers.get(j).getEmail());
+                    tek=false;
+                    response.sendRedirect("/SopraCarpooling-war/adminhome");
+                }
+            }
+            if (tek){
+            this.getServletContext().getRequestDispatcher("/WEB-INF/adminhome.jsp").forward(request, response);
+            }
         }
-        else if (request.getParameter("mod1")!=null){
-            this.getServletContext().getRequestDispatcher("/WEB-INF/modpersinfo.jsp").forward(request, response);}
-        else if (request.getParameter("mod2")!=null){
-            response.sendRedirect("/SopraCarpooling-war/persinfo");}
-        else if (request.getParameter("mod3")!=null){
-            response.sendRedirect("/SopraCarpooling-war/persinfo");}
-        else { 
-        this.getServletContext().getRequestDispatcher("/WEB-INF/adminhome.jsp").forward(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(HomeAdminManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }	
-
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -74,23 +95,37 @@ public class HomeAdminManager extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Cookie[] cookies = request.getCookies();
-        String Valeur= null;
-        if (cookies!=null){
-        for(int i=0; i < cookies.length; i++) {
-		Cookie MonCookie = cookies[i];
+        String Valeur = null;
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie MonCookie = cookies[i];
                 if (MonCookie.getName().equals("admin")) {
-			Valeur = cookies[i].getValue();
+                    Valeur = cookies[i].getValue();
                 }
+            }
         }
-        }
-        
-        if (Valeur==null){
+        if (Valeur == null) {
             response.sendRedirect("/SopraCarpooling-war/login");
-        }else{
-            int positionAt = Valeur.indexOf("@#**#@");
-            String email = Valeur.substring(0, positionAt);
-            String password = Valeur.substring(positionAt+6);
+        } else {
+            update(request, response);
+        }
+    }
+
+    public void update(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Connection con;
+            con = DatabaseManager.connectionDatabase();
+            ArrayList<Model.User> listUsers = DatabaseManager.getAllUsers(con);
+            ArrayList<String> listWk = DatabaseManager.getAllWorkplaces(con);
+            request.setAttribute("listUsers", listUsers);
+            request.setAttribute("work", listWk);
             processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(HomeAdminManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ServletException ex) {
+            Logger.getLogger(HomeAdminManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(HomeAdminManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
