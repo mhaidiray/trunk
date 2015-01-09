9babdf001d4b66d5c78c081378259b832799a87a
orosens@gmail.com <orosens@gmail.com@82b39981-8c08-d3b6-9424-b9cea07f54b6>
2015-01-09 22:50:48 +0000

    gestroutes fini+CHANGEMENTS ROUTE ET DATABASEMANAGER

--- trunk/SopraCarpooling/SopraCarpooling-war/src/java/Model/Route.java
+++ trunk/SopraCarpooling/SopraCarpooling-war/src/java/Model/Route.java
@@ -5,18 +5,54 @@
  */
 package Model;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Machd
  */
 public class Route {
-    
+
     private String workplace;
+
+    public int getNbcond() {
+        return nbcond;
+    }
+
+    public int getNbpass() {
+        return nbpass;
+    }
+    
     private int zipcode;
+    private int nbcond;
+    private int nbpass;
 
     public Route(int zipcode, String workplace) {
         this.workplace = workplace;
         this.zipcode = zipcode;
+        nbcond = 0;
+        nbpass = 0;
+        Connection con;
+        try {
+            con = DatabaseManager.connectionDatabase();
+            ArrayList<Model.User> listUser = DatabaseManager.getAllUsers(con);
+            ArrayList<String> listPlaces = DatabaseManager.getAllWorkplaces(con);
+            for (Model.User u : listUser) {
+                if (zipcode == u.getZipcode() && workplace.equals(listPlaces.get(u.getWorkplace()-1))) {
+                    if (u.getDriver() == 1) {
+                        nbcond++;
+                    } else {
+                        nbpass++;
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Route.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public String getWorkplace() {
@@ -34,7 +70,5 @@ public class Route {
     public void setZipcode(int zipcode) {
         this.zipcode = zipcode;
     }
-    
-    
-    
+
 }
