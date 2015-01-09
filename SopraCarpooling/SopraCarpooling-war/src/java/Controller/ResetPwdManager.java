1cffda640b32de8d90206cd4ff3757576be99ebf
squallih.mohamed <squallih.mohamed@gmail.com>
2015-01-09 22:02:01 +0000

    Chiffrage du mot de passe (avec MD5)

--- SopraCarpooling/SopraCarpooling-war/src/java/Controller/ResetPwdManager.java
+++ SopraCarpooling/SopraCarpooling-war/src/java/Controller/ResetPwdManager.java
@@ -5,11 +5,18 @@
  */
 package Controller;
 
+import Model.DatabaseManager;
 import Model.SMTPManager;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -52,7 +59,35 @@ public class ResetPwdManager extends HttpServlet {
         /* TODO output your page here. You may use following sample code. */
         this.getServletContext().getRequestDispatcher("/WEB-INF/resetpwd.jsp").forward(request, response);
     }
+    
+    private static String encode(String mdp, String email){
+                    String password = mdp+"SopraCarpooling"+email;
+                    byte[] uniqueKey = password.getBytes();
+                    byte[] hash      = null;
 
+                    try
+                    {
+                        hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
+                    }
+                    catch (NoSuchAlgorithmException e)
+                    {
+                        throw new Error("No MD5 support in this VM.");
+                    }
+
+                    StringBuilder hashString = new StringBuilder();
+                    for (int i = 0; i < hash.length; i++)
+                    {
+                        String hex = Integer.toHexString(hash[i]);
+                        if (hex.length() == 1)
+                        {
+                            hashString.append('0');
+                            hashString.append(hex.charAt(hex.length() - 1));
+                        }
+                        else
+                            hashString.append(hex.substring(hex.length() - 2));
+                    }
+                    return hashString.toString();
+        }  
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -79,19 +114,24 @@ public class ResetPwdManager extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String email = request.getParameter("mail");
-        Boolean exist = true;
-        checkMail(request,response);
-        
-        /**Vérification que l'email existe dans la base de données*/
-        if (exist && erreurs.isEmpty()){
-            SecureRandom random = new SecureRandom();
-            String pwd = new BigInteger(60, random).toString(32);
-            /**Modification du password dans la base de données*/
-            SMTPManager.sendNewPassword(email, pwd);
-        } else {
-            this.getServletContext().getRequestDispatcher("/WEB-INF/resetpwd.jsp").forward(request, response);
-            erreurs.clear();
+        try {
+            String email = request.getParameter("mail");
+            Connection con = DatabaseManager.connectionDatabase();
+            Boolean exist = DatabaseManager.existMail(con, email);
+            checkMail(request,response);
+            
+            /**Vérification que l'email existe dans la base de données*/
+            if (exist && erreurs.isEmpty()){
+                SecureRandom random = new SecureRandom();
+                String pwd = new BigInteger(60, random).toString(32);
+                /**Modification du password dans la base de données*/
+                SMTPManager.sendNewPassword(email, encode(pwd,email));
+            } else {
+                this.getServletContext().getRequestDispatcher("/WEB-INF/resetpwd.jsp").forward(request, response);
+                erreurs.clear();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ResetPwdManager.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         
